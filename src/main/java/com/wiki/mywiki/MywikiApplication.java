b9c1d71f9825ab79b0aea08f149bfbfdf6cb529e
Stella313 <silverdew313@gmail.com>
2022-06-11 17:55:07 -0400

    Update startup log

--- src/main/java/com/wiki/mywiki/MywikiApplication.java
+++ src/main/java/com/wiki/mywiki/MywikiApplication.java
@@ -1,13 +1,20 @@
 package com.wiki.mywiki;
 
+import org.springframework.core.env.Environment;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 @SpringBootApplication
 public class MywikiApplication {
-
+    private static final Logger LOG = LoggerFactory.getLogger(MywikiApplication.class);
     public static void main(String[] args) {
-        SpringApplication.run(MywikiApplication.class, args);
+        SpringApplication app = new SpringApplication(MywikiApplication.class);
+        Environment env = app.run(args).getEnvironment();
+        LOG.info("Start successfully!! ");
+        LOG.info("Address: \thttp://127.0.0.1:{}", env.getProperty("sever.port"));
     }
 
 }
