6960074b3de0a27b004f2605a0a32f6094e375f2
Stella313 <silverdew313@gmail.com>
2022-06-11 20:33:37 -0400

    Modify the position of config file and set some configs

--- src/main/java/com/wiki/mywiki/controller/TestController.java
+++ src/main/java/com/wiki/mywiki/controller/TestController.java
@@ -1,14 +1,18 @@
 package com.wiki.mywiki.controller;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestController {
+    @Value("${test.hello:TEST}")
+    private String testH;
+
     @GetMapping("/hello")
     public String hello(){
-        return "Hello World!";
+        return "Hello World!" + " " + testH;
     }
 
     @PostMapping("/hello/post")
