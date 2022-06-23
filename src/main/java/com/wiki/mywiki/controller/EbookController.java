922dbe1680a0c075ac2851f1ed30193f41749bd7
Stella313 <silverdew313@gmail.com>
2022-06-23 01:01:28 -0400

    Add the common response class

--- src/main/java/com/wiki/mywiki/controller/EbookController.java
+++ src/main/java/com/wiki/mywiki/controller/EbookController.java
@@ -1,6 +1,7 @@
 package com.wiki.mywiki.controller;
 
 import com.wiki.mywiki.domain.Ebook;
+import com.wiki.mywiki.resp.CommonResp;
 import com.wiki.mywiki.service.EbookService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -17,8 +18,11 @@ public class EbookController {
     private EbookService ebookService;
 
     @GetMapping("list")
-            public List<Ebook> list(){
-        return ebookService.list();
+    public CommonResp list(){
+        CommonResp<List<Ebook>> resp = new CommonResp<>();
+        List<Ebook> list =  ebookService.list();
+        resp.setContent(list);
+        return resp;
     }
 
 }
