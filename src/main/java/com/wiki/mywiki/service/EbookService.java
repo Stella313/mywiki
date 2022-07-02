a29b0ec234080d18e99c42aa5e713d028e9b83c2
Stella313 <silverdew313@gmail.com>
2022-07-02 00:17:42 -0400

    Updated ebook service Adjust the style of the cover image

--- src/main/java/com/wiki/mywiki/service/EbookService.java
+++ src/main/java/com/wiki/mywiki/service/EbookService.java
@@ -8,6 +8,7 @@ import com.wiki.mywiki.resp.EbookResp;
 import com.wiki.mywiki.util.CopyUtil;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.ObjectUtils;
 
 import javax.annotation.Resource;
 import java.util.ArrayList;
@@ -23,7 +24,9 @@ public class EbookService {
     public List<EbookResp> list(EbookReq req){
         EbookExample ebookExample = new EbookExample();
         EbookExample.Criteria criteria = ebookExample.createCriteria();
-        criteria.andNameLike("%" + req.getName() + "%");
+        if(!ObjectUtils.isEmpty(req.getName())){
+            criteria.andNameLike("%" + req.getName() + "%");
+        }
         List<Ebook> ebookList =  ebookMapper.selectByExample(ebookExample);
         return CopyUtil.copyList(ebookList, EbookResp.class);
     }
