bb6af3b5113c151e290a0487f4af86ab715497aa
Stella313 <silverdew313@gmail.com>
2022-07-02 15:46:38 -0400

    配置axios拦截器打印请求参数和返回参数

--- web/src/views/Home.vue
+++ web/src/views/Home.vue
@@ -1,71 +1,75 @@
 <template>
   <a-layout>
-    <a-layout-sider width="200" style="background: #fff">
-      <a-menu
-          mode="inline"
-          v-model:selectedKeys="selectedKeys2"
-          v-model:openKeys="openKeys"
-          :style="{height: '100%', borderRight: 0}"
-      >
-        <a-sub-menu key="sub1">
-          <template #title>
+    <a-layout-content style="padding: 0 50px">
+      <a-layout style="padding: 24px 0; background: #fff">
+        <a-layout-sider width="200" style="background: #fff">
+          <a-menu
+              mode="inline"
+              v-model:selectedKeys="selectedKeys2"
+              v-model:openKeys="openKeys"
+              :style="{height: '100%', borderRight: 0}"
+          >
+            <a-sub-menu key="sub1">
+              <template #title>
                 <span>
                   <user-outlined />
                   subnav 1
                 </span>
-          </template>
-          <a-menu-item key="1">option1</a-menu-item>
-          <a-menu-item key="2">option2</a-menu-item>
-          <a-menu-item key="3">option3</a-menu-item>
-          <a-menu-item key="4">option4</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub2">
-          <template #title>
+              </template>
+              <a-menu-item key="1">option1</a-menu-item>
+              <a-menu-item key="2">option2</a-menu-item>
+              <a-menu-item key="3">option3</a-menu-item>
+              <a-menu-item key="4">option4</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub2">
+              <template #title>
                 <span>
                   <laptop-outlined />
                   subnav 2
                 </span>
-          </template>
-          <a-menu-item key="5">option5</a-menu-item>
-          <a-menu-item key="6">option6</a-menu-item>
-          <a-menu-item key="7">option7</a-menu-item>
-          <a-menu-item key="8">option8</a-menu-item>
-        </a-sub-menu>
-        <a-sub-menu key="sub3">
-          <template #title>
+              </template>
+              <a-menu-item key="5">option5</a-menu-item>
+              <a-menu-item key="6">option6</a-menu-item>
+              <a-menu-item key="7">option7</a-menu-item>
+              <a-menu-item key="8">option8</a-menu-item>
+            </a-sub-menu>
+            <a-sub-menu key="sub3">
+              <template #title>
                 <span>
                   <notification-outlined />
                   subnav 3
                 </span>
-          </template>
-          <a-menu-item key="9">option9</a-menu-item>
-          <a-menu-item key="10">option10</a-menu-item>
-          <a-menu-item key="11">option11</a-menu-item>
-          <a-menu-item key="12">option12</a-menu-item>
-        </a-sub-menu>
-      </a-menu>
-    </a-layout-sider>
-    <a-layout-content
-        :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px' }"
-    >
-      <a-list item-layout="vertical" size="large" :grid="{gutter: 20, column: 3}"  :data-source="ebooks">
-        <template #renderItem="{ item }">
-          <a-list-item key="item.name">
-            <template #actions>
+              </template>
+              <a-menu-item key="9">option9</a-menu-item>
+              <a-menu-item key="10">option10</a-menu-item>
+              <a-menu-item key="11">option11</a-menu-item>
+              <a-menu-item key="12">option12</a-menu-item>
+            </a-sub-menu>
+          </a-menu>
+        </a-layout-sider>
+        <a-layout-content
+            :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px'}"
+        >
+          <a-list item-layout="vertical" size="large" :grid="{gutter: 20, column: 3}"  :data-source="ebooks">
+            <template #renderItem="{ item }">
+              <a-list-item key="item.name">
+                <template #actions>
           <span v-for="{ type, text } in actions" :key="type">
             <component v-bind:is="type" style="margin-right: 8px" />
             {{ text }}
           </span>
+                </template>
+                <a-list-item-meta :description="item.description">
+                  <template #title>
+                    <a :href="item.href">{{ item.name }}</a>
+                  </template>
+                  <template #avatar><a-avatar :src="item.cover" /></template>
+                </a-list-item-meta>
+              </a-list-item>
             </template>
-            <a-list-item-meta :description="item.description">
-              <template #title>
-                <a :href="item.href">{{ item.name }}</a>
-              </template>
-              <template #avatar><a-avatar :src="item.cover" /></template>
-            </a-list-item-meta>
-          </a-list-item>
-        </template>
-      </a-list>
+          </a-list>
+        </a-layout-content>
+    </a-layout>
     </a-layout-content>
   </a-layout>
 </template>
@@ -91,16 +95,13 @@ for (let i = 0; i < 23; i++) {
 export default defineComponent({
   name: 'Home',
   setup(){
-    console.log("setup");
     const ebooks = ref();
     const ebooks1 = reactive({books: []});
     onMounted(()=>{
-      console.log("onMounted");
       axios.get("/ebook/list").then((response) =>{
         const data = response.data;
         ebooks.value = data.content;
         ebooks1.books = data.content;
-        console.log(response);
       })
     })
     return{
