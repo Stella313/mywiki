85e61761ad6f93e1e6dc03466d26eda0f5dea767
Stella313 <silverdew313@gmail.com>
2022-07-06 01:27:46 -0400

    集成pageHelper

--- src/main/java/com/wiki/mywiki/service/EbookService.java
+++ src/main/java/com/wiki/mywiki/service/EbookService.java
@@ -1,11 +1,15 @@
 package com.wiki.mywiki.service;
 
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
 import com.wiki.mywiki.domain.Ebook;
 import com.wiki.mywiki.domain.EbookExample;
 import com.wiki.mywiki.mapper.EbookMapper;
 import com.wiki.mywiki.req.EbookReq;
 import com.wiki.mywiki.resp.EbookResp;
 import com.wiki.mywiki.util.CopyUtil;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.BeanUtils;
 import org.springframework.stereotype.Service;
 import org.springframework.util.ObjectUtils;
@@ -16,6 +20,7 @@ import java.util.List;
 
 @Service
 public class EbookService {
+    private static final Logger LOG = LoggerFactory.getLogger(EbookService.class);
     @Resource
     private EbookMapper ebookMapper;
     public List<Ebook> list(){
@@ -27,7 +32,11 @@ public class EbookService {
         if(!ObjectUtils.isEmpty(req.getName())){
             criteria.andNameLike("%" + req.getName() + "%");
         }
+        PageHelper.startPage(1, 3);
         List<Ebook> ebookList =  ebookMapper.selectByExample(ebookExample);
+        PageInfo<Ebook> pageInfo = new PageInfo<>(ebookList);
+        LOG.info("总行数：{}",pageInfo.getTotal());
+        LOG.info("总页数：{}",pageInfo.getPages());
         return CopyUtil.copyList(ebookList, EbookResp.class);
     }
 }
