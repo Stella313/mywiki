697f0c239bc15c97e217faec0d0fb09057219579
Stella313 <silverdew313@gmail.com>
2022-07-14 00:50:03 -0400

    增加保存文档成功提示 编辑和新增的时候，先清空富文本框

--- web/src/views/admin/admin-doc.vue
+++ web/src/views/admin/admin-doc.vue
@@ -190,8 +190,8 @@ export default defineComponent({
         modalLoading.value = false;
         const data = response.data; // data = commonResp
         if(data.success ){
-          modalVisible.value = false;
-
+          // modalVisible.value = false;
+          message.success("保存成功！");
           // 重新加载列表
           handleQuery();
         }else{
@@ -276,6 +276,8 @@ export default defineComponent({
      * 编辑
      */
     const edit = (record: any) => {
+      // 清空富文本框
+      editor.txt.html("");
       modalVisible.value = true;
       doc.value = Tool.copy(record);
       handleQueryContent();
@@ -287,6 +289,8 @@ export default defineComponent({
      * 新增
      */
     const add = () => {
+      // 清空富文本框
+      editor.txt.html("");
       modalVisible.value = true;
       doc.value = {
         ebookId: route.query.ebookId
