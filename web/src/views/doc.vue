c132dc0f5125c4d82624d8bfa6bd77b56939563a
Stella313 <silverdew313@gmail.com>
2022-07-15 12:21:59 -0400

    打开文档页面时，如果没有文档，增加提示 BUG修复，初始打开文档管理页面，直接新增文档时会报错：电子书不能为空

--- web/src/views/doc.vue
+++ web/src/views/doc.vue
@@ -3,6 +3,7 @@
     <a-layout-content
         :style="{ background: '#fff', padding: '24px', margin: 0, minHeight: '280px'}"
     >
+      <h3 v-if="level1.length === 0">对不起，找不到相关文档！</h3>
       <a-row>
         <a-col :spam="6">
           <a-tree
