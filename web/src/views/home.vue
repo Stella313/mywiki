b965b2c34f83a6c19c9f226092a0227364cda3f9
Stella313 <silverdew313@gmail.com>
2022-07-15 02:18:41 -0400

    点击文档节点时，加载并显示文档内容

--- web/src/views/home.vue
+++ web/src/views/home.vue
@@ -40,7 +40,7 @@
                 </template>
                 <a-list-item-meta :description="item.description">
                   <template #title>
-                    <router-link to="'/doc?ebookId=' + item.id">
+                    <router-link :to="'/doc?ebookId=' + item.id">
                       {{item.name}}
                     </router-link>
                   </template>
