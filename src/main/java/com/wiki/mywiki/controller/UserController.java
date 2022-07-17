bc49a5a3f34ceec3dbd04da930ed218ff0e6d97c
Stella313 <silverdew313@gmail.com>
2022-07-17 16:12:40 -0400

    集成redis,登录成功后将登录信息放入redis中

--- src/main/java/com/wiki/mywiki/controller/UserController.java
+++ src/main/java/com/wiki/mywiki/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.wiki.mywiki.controller;
 
+import com.alibaba.fastjson.JSONObject;
 import com.wiki.mywiki.req.UserLoginReq;
 import com.wiki.mywiki.req.UserQueryReq;
 import com.wiki.mywiki.req.UserResetPasswordReq;
@@ -9,17 +10,27 @@ import com.wiki.mywiki.resp.UserLoginResp;
 import com.wiki.mywiki.resp.UserQueryResp;
 import com.wiki.mywiki.resp.PageResp;
 import com.wiki.mywiki.service.UserService;
+import com.wiki.mywiki.util.UuidUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
+import java.util.concurrent.TimeUnit;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    final Logger LOG = LoggerFactory.getLogger(UserController.class);
     @Resource
     private UserService userService;
+    @Resource
+    private RedisTemplate redisTemplate;
+    @Resource
+    private UuidUtils uuidUtils;
 
     @GetMapping("/list")
     public CommonResp list(@Valid UserQueryReq req){
@@ -55,6 +66,10 @@ public class UserController {
         req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp<UserLoginResp> resp = new CommonResp<>();
         UserLoginResp userLoginResp = userService.login(req);
+        Long token = uuidUtils.getId();
+        LOG.info("生成单点登录token: {}，并放入redis中",token);
+        userLoginResp.setToken(token.toString());
+        redisTemplate.opsForValue().set(token, JSONObject.toJSONString(userLoginResp), 3600 * 24, TimeUnit.SECONDS);
         resp.setContent(userLoginResp);
         return resp;
     }
--- src/main/java/com/wiki/mywiki/resp/UserLoginResp.java
+++ src/main/java/com/wiki/mywiki/resp/UserLoginResp.java
@@ -5,10 +5,27 @@ import javax.validation.constraints.Pattern;
 
 public class UserLoginResp {
     private Long id;
-    @NotNull(message = "【用户名】不能为空")
     private String loginName;
-    @NotNull(message = "【昵称】不能为空")
     private String name;
+    private String token;
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+
+    @Override
+    public String toString() {
+        return "UserLoginResp{" +
+                "id=" + id +
+                ", loginName='" + loginName + '\'' +
+                ", name='" + name + '\'' +
+                ", token='" + token + '\'' +
+                '}';
+    }
 
     public Long getId() {
         return id;
@@ -34,16 +51,4 @@ public class UserLoginResp {
         this.name = name;
     }
 
-    @Override
-    public String toString() {
-        StringBuilder sb = new StringBuilder();
-        sb.append(getClass().getSimpleName());
-        sb.append(" [");
-        sb.append("Hash = ").append(hashCode());
-        sb.append(", id=").append(id);
-        sb.append(", loginName=").append(loginName);
-        sb.append(", name=").append(name);
-        sb.append("]");
-        return sb.toString();
-    }
 }
