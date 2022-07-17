9c5a423ef7246dfac4f800778dd6be8a79a4760b
Stella313 <silverdew313@gmail.com>
2022-07-17 01:04:42 -0400

    密码加密传输和加密存储

--- src/main/java/com/wiki/mywiki/controller/UserController.java
+++ src/main/java/com/wiki/mywiki/controller/UserController.java
@@ -6,6 +6,7 @@ import com.wiki.mywiki.resp.CommonResp;
 import com.wiki.mywiki.resp.UserQueryResp;
 import com.wiki.mywiki.resp.PageResp;
 import com.wiki.mywiki.service.UserService;
+import org.springframework.util.DigestUtils;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
@@ -26,6 +27,7 @@ public class UserController {
     }
     @PostMapping("/save")
     public CommonResp save(@Valid @RequestBody UserSaveReq req){
+        req.setPassword(DigestUtils.md5DigestAsHex(req.getPassword().getBytes()));
         CommonResp resp = new CommonResp<>();
         userService.save(req);
         return resp;
